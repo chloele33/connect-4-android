75c72ab8eafe132b27eda820b16330fc7bc240a0
chloele33 <chloele@seas.upenn.edu>
2018-10-28 16:45:59 -0400

    splash page commit

---
+++ app/src/main/java/edu/upenn/chloele/connect4/SplashActivity.kt
@@ -0,0 +1,29 @@
+package edu.upenn.chloele.connect4
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+
+
+
+class SplashActivity : AppCompatActivity() {
+
+    /** Splash page display duration */
+    private val SPLASH_DISPLAY_LENGTH: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        /** Hander to delay mainActivity for splash display length amount of seconds */
+        Handler().postDelayed(Runnable {
+            /* Create an Intent that will start the Menu-Activity. */
+            val mainIntent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, SPLASH_DISPLAY_LENGTH)
+
+    }
+}
