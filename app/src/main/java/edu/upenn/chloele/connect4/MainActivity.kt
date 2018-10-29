079bae91a5d6d7a0356a3f2e46705e099f2c56c5
chloele33 <chloele@seas.upenn.edu>
2018-10-28 22:32:25 -0400

    board layout commit

--- app/src/main/java/edu/upenn/chloele/connect4/MainActivity.kt
+++ app/src/main/java/edu/upenn/chloele/connect4/MainActivity.kt
@@ -3,6 +3,8 @@ package edu.upenn.chloele.connect4
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +15,16 @@ class MainActivity : AppCompatActivity() {
         val intent = getIntent()
         val player1Name = intent.getStringExtra("Player1Name")
         val player2Name = intent.getStringExtra("Player2Name")
+        val player1Color = "#ff4911"
+        val player2Color = "#ffd711"
+        val boardMatrix = Array(6, {IntArray(7)})
 
         TextPlayer1Name.setText(player1Name)
         TextPlayer2Name.setText(player2Name)
         currPlayerName.setText(player1Name)
-        currPlayerName.setTextColor(Color.parseColor("#ff4911"))
+        currPlayerName.setTextColor(Color.parseColor(player1Color))
+        val currPlayerColor = player1Color
+
+
     }
 }
