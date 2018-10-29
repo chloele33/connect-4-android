5577b0a1f86e262ce813cacc0c90a1217dfa0090
chloele33 <chloele@seas.upenn.edu>
2018-10-29 01:24:32 -0400

    check winning

--- app/src/main/java/edu/upenn/chloele/connect4/MainActivity.kt
+++ app/src/main/java/edu/upenn/chloele/connect4/MainActivity.kt
@@ -60,13 +60,23 @@ class MainActivity : AppCompatActivity() {
             _count++
             // toggle player
             if (_currPlayer == 1) {
-                _boardMatrix[rowNum][colNum] = 1
+                _boardMatrix[-1 * (rowNum - 5)][colNum] = 1
+                if (checkIfWon(1)) {
+                    val toast = Toast.makeText(applicationContext,
+                            "PLAYER 1 WON", Toast.LENGTH_SHORT)
+                    toast.show()
+                }
                 _currPlayer = 2
                 _currPlayerColor = _player2Color
                 currPlayerName.setText(_player2Name)
 
             } else {
-                _boardMatrix[rowNum][colNum] = 2
+                _boardMatrix[-1 * (rowNum - 5)][colNum] = 2
+                if (checkIfWon(2)) {
+                    val toast = Toast.makeText(applicationContext,
+                            "PLAYER 2 WON", Toast.LENGTH_SHORT)
+                    toast.show()
+                }
                 _currPlayer = 1
                 _currPlayerColor = _player1Color
                 currPlayerName.setText(_player1Name)
@@ -79,7 +89,36 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun checkIfWon(playerNum : Int) {
+    private fun checkIfWon(playerNum : Int) : Boolean {
+        for (col in 0 until 7) {
+            if (connect(playerNum, 0, 1, col, 0, 0)
+                    || connect(playerNum, 1, 1, col, 0, 0)
+                    || connect(playerNum, -1, 1, col, 0, 0)) {
+                return true
+            }
+        }
+        for (row in 0 until 6) {
+            if (connect(playerNum, 1, 0, 0, row, 0)
+                    || connect(playerNum, 1, 1, 0, row, 0)
+                    || connect(playerNum, -1, 1, 7 - 1, row, 0)) {
+                return true
+            }
+        }
+        return false
+    }
 
+    private fun connect(playerNum : Int, dirX : Int, dirY : Int, col : Int, row : Int, totalConnected : Int) : Boolean {
+        if (totalConnected >= 4) {
+            return true
+        }
+        // check if out of bounds
+        if (row < 0 || col < 0 || col >= 7 || row >= 6) {
+            return false
+        }
+        if (_boardMatrix[row][col].toInt() == playerNum) {
+            return connect(playerNum, dirX, dirY, col + dirX, row + dirY, totalConnected + 1);
+        } else {
+            return connect(playerNum, dirX, dirY, col + dirX, row + dirY, 0);
+        }
     }
 }
